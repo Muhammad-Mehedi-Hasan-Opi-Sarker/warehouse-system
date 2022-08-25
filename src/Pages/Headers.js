98051bf9434d21fcf1085b4b761398b5ad9d2524
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-25 10:53:59 +0600

    first commit

---
+++ src/Pages/Headers.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Headers = () => {
+    const nav = <>
+        <li><Link to='/home'>Home</Link></li>
+        <li><Link to='/services'>Services</Link></li>
+        <li><Link to='/services'>Shop</Link></li>
+        <li><Link to='/services'>Items</Link></li>
+        <li><Link to='/services'>Add Item</Link></li>
+        <li><Link to='about'>About</Link></li>
+        <li><Link to='/blog' >Blog</Link></li>
+    </>
+ 
+    return (
+        <div>
+            <div className="navbar text-white" style={{backgroundColor:"#232331"}}>
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabindex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {nav}
+                        </ul>
+                    </div>
+                    <Link to='/home' className="btn btn-ghost normal-case text-xl"><h1
+                        className='font-bold text-2xl'>WareHouse</h1></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        {nav}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <ul className="menu menu-horizontal p-0">
+                        <li> <Link to='/login'>login</Link></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Headers;
---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Banner.css';
+import banner from '../../assets/banner2.jpg';
+import banner2 from '../../assets/banner1.jpg';
+
+const Banner = () => {
+    return (
+        <div>
+            <div class="carousel w-full">
+                <div id="slide1" class="carousel-item relative w-full height">
+                    <img src={banner} alt="" class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" class="btn btn-circle">❮</a>
+                        <a href="#slide2" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" class="carousel-item relative w-full height">
+                    <img src={banner2} alt="" class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" class="btn btn-circle">❮</a>
+                        <a href="#slide1" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" class="carousel-item relative w-full height">
+                    <img src={banner} alt="" class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" class="btn btn-circle">❮</a>
+                        <a href="#slide1" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
