a825a69d1d407f5a43db8e9b68345a1ecf5bbc98
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-25 17:14:01 +0600

    add form

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -6,29 +6,36 @@ import banner2 from '../../assets/banner1.jpg';
 const Banner = () => {
     return (
         <div className=''>
-            <div className="carousel w-full mb-8">
-                <div id="slide1" className="carousel-item relative w-full height">
-                    <img src={banner} alt="" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
+            {/* <div className="carousel w-full mb-8"> */}
+            <div class="carousel w-full">
+                <div id="slide1" class="carousel-item relative w-full height">
+                    <img src={banner} alt='' class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" class="btn btn-circle">❮</a>
+                        <a href="#slide2" class="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide2" className="carousel-item relative w-full height">
-                    <img src={banner2} alt="" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
+                <div id="slide2" class="carousel-item relative w-full height">
+                    <img src={banner2} alt='' class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" class="btn btn-circle">❮</a>
+                        <a href="#slide3" class="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide3" className="carousel-item relative w-full height">
-                    <img src={banner} alt="" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
+                <div id="slide3" class="carousel-item relative w-full height">
+                    <img src={banner} alt='' class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" class="btn btn-circle">❮</a>
+                        <a href="#slide4" class="btn btn-circle">❯</a>
                     </div>
                 </div>
-
+                <div id="slide4" class="carousel-item relative w-full height">
+                    <img src={banner2} alt='' class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" class="btn btn-circle">❮</a>
+                        <a href="#slide1" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Home/Form.js
+++ src/Pages/Home/Form.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import './Form.css';
 
 const Form = () => {
     return (
-        <div className='mt-12 lg:ml-96'>
-            <h1 className='font-bold text-3xl mb-12'>REQUEST A QUICK QUOTE</h1>
-            <p className='mb-8'>We love to listen and we are eagerly waiting to talk to you 
-                regarding your project. Get in touch with us if you have any
-                 queries and we will get back to you as soon as possible.</p>
-                 <form className=''>
-                 <input name='name' type="text" placeholder="Type here" className="input mb-2 input-bordered input-info w-full max-w-xs" />
-                 <input name='subject' type="text" placeholder="Type here" className="ml-5 mb- input input-bordered input-info w-full max-w-xs" /><br />
-                 <input name='email' type="email" placeholder="Type here" className="mb-2 input input-bordered input-info w-full max-w-xs" />
-                 <input name='phone' type="phone" placeholder="Type here" className="mb-2 ml-5 input input-bordered input-info w-fit max-w-xs" /><br />
-                    <input type="text" placeholder="Type here" className="mb-2 input input-bordered input-lg w-full max-w-xs" /> <br />
-                 <input className='mb-5 btn' type="submit" value="Send Message" />
-                 </form>
+        <div className='p-5 lg:pl-8 lg:py-8 px-4 bg-center bg-[url("https://transport.thememove.com/wp-content/uploads/2015/06/parallax021.jpg")]'>
+            <div className='grid lg:ml-0 ml-8 p-0'>
+            <div className='res bg-slate-800'>
+               <div className='p-5'>
+               <h1 className='font-bold text-3xl mb-12 text-white'>REQUEST A QUICK QUOTE</h1>
+                <p className='mb-8 text-white'>We love to listen and we are eagerly waiting to talk to you
+                    regarding your project. Get in touch with us if you have any
+                    queries and we will get back to you as soon as possible.</p>
+                <form>
+                    <input name='name' type="text" placeholder="Your name" className="input mb-2 input-bordered input-info w-full max-w-xs" />
+                    <input name='subject' type="text" placeholder="Subject" className="lg:ml-5 mb-2 input input-bordered input-info w-full max-w-xs" /><br />
+                    <input name='email' type="email" placeholder="Your email" className="mb-2 input input-bordered input-info w-full max-w-xs" />
+                    <input name='phone' type="phone" placeholder="Phone" className="mb-2 lg:ml-5 input input-bordered input-info w-full max-w-xs" /><br />
+                    <input type="text" placeholder="Your message" className="mb-2 input input-bordered input-lg w-full max-w-xs" /> <br />
+                    <input className='mb-5 btn' type="submit" value="Send Message" />
+                </form>
+               </div>
+            </div>
+        </div>
         </div>
     );
 };
