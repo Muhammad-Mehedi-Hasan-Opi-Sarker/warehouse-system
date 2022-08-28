871f97efc61965ed7857d4864b187cdc59960dc6
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-28 17:36:49 +0600

    add single product

--- src/Pages/Home/Shop/Product.js
+++ src/Pages/Home/Shop/Product.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Product = ({ product }) => {
+    const navigate = useNavigate();
+    const handleOrder = id => {
+        navigate(`/inventory/${id}`)
+    }
     return (
         <div class="card card-compact w-96 bg-base-100 shadow rounded-none mb-5">
             <figure><img className='w-56 h-56' src={product.image} alt="Shoes" /></figure>
             <div class="card-body">
                 <p className='font-bold'>{product.name}</p>
                 <h4 className='font-bold text-2xl'>$ {product.price}</h4>
-                <button className='btn rounded-none border-none' style={{backgroundColor:'#CA1F26'}}>Order</button>
+                <button onClick={() => handleOrder(product._id)} className='btn rounded-none border-none' style={{ backgroundColor: '#CA1F26' }}>Order</button>
             </div>
         </div>
     );
--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Inventory = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+        const url = `http://localhost:5000/product/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProduct(data));
+    }, [])
     return (
         <div>
+            <h1 className='font-bold text-5xl'>{product.name}</h1>
             
+            {/* <p>{product.name}</p> */}
         </div>
     );
 };
