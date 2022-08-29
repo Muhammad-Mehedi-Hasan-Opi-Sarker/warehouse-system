8844e14a38d6002072259200693d72dc7ad7a982
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-29 13:47:12 +0600

    complite footer section

--- src/Pages/Items/Items.js
+++ src/Pages/Items/Items.js
@@ -1,13 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import Footer from '../Shared/Footer';
 
 const Items = () => {
     const [items, setItems] = useState([]);
+    const [reload, setReload] = useState(false);
     useEffect(() => {
         fetch("http://localhost:5000/product")
             .then(res => res.json()).then(data => setItems(data));
-    }, [])
+    }, [reload])
+
+    const handleDelete = id => {
+        console.log(id)
+        const proceed = window.confirm("Are you sure");
+        if (proceed) {
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setReload(!reload)
+                })
+        }
+    }
+
     return (
-        <div className='grid justify-items-center mt-8'>
+        <div>
+            <div className='grid justify-items-center mt-8'>
             {/* <h1 className='font-bold text-4xl'>Your Items List. </h1> */}
             <h1 className='font-bold text-3xl mt-5 text-red-600'> Dangerous Zone</h1>
             {
@@ -17,13 +36,14 @@ const Items = () => {
                         <tbody>
                             <tr class="hover">
                                 <td>{item.name}</td>
-                                <td><button className='btn'>Delete</button></td>
+                                <td><button onClick={() => handleDelete(item._id)} className='btn'>Delete</button></td>
                             </tr>
                         </tbody>
                     </table>
                 </div>)
             }
-
+        </div>
+        <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,50 +1,54 @@
 import React from 'react';
+import { AiFillFacebook,AiOutlineTwitter,AiOutlineGooglePlus,AiFillLinkedin,AiOutlineArrowRight } from 'react-icons/ai';
 
 const Footer = () => {
     return (
         <div>
-            <footer class="footer grid-rows-2 p-10 bg-neutral text-neutral-content">
+            <footer class="footer grid lg:grid-cols-3 grid-justify-items-center p-10 bg-neutral text-neutral-content">
                 <div>
-                    <span class="footer-title">Services</span>
-                    <a class="link link-hover">Branding</a>
-                    <a class="link link-hover">Design</a>
-                    <a class="link link-hover">Marketing</a>
-                    <a class="link link-hover">Advertisement</a>
+                    <span style={{ backgroundColor: '#CA1F26' }} class="mb-5 w-56 h-22 p-3 font-bold text-xl text-white">ABOUT US</span>
+                    <img className='mb-5' src="https://transport.thememove.com/data/images/logo.png" alt="" />
+                    <p className='mb-5'>Transport offers a host of logistic management services and supply chain solutions. We provide
+                        innovative solutions with the best people, processes, and technology.</p>
+                        <div className='grid grid-cols-4 gap-5 text-xl'>
+                            <AiOutlineTwitter></AiOutlineTwitter>
+                            <AiOutlineGooglePlus></AiOutlineGooglePlus>
+                            <AiFillLinkedin></AiFillLinkedin>
+                            <AiFillFacebook></AiFillFacebook>
+                        </div>
                 </div>
-                <div>
-                    <span class="footer-title">Company</span>
-                    <a class="link link-hover">About us</a>
-                    <a class="link link-hover">Contact</a>
-                    <a class="link link-hover">Jobs</a>
-                    <a class="link link-hover">Press kit</a>
+
+                <div className='lg:ml-16'>
+                    <span style={{ backgroundColor: '#CA1F26' }} class="mb-5 w-56 h-22 p-3 font-bold text-xl text-white">INFORMATION</span>
+                    <div className='flex'>
+                        <div>
+                           
+                            <a class="link link-hover flex"><span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span> About Us</a> <br />
+                            <a class="link link-hover flex"><span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span>Blog</a><br />
+                            <a class="link link-hover flex"><span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span>Checkout</a><br />
+                            <a class="link link-hover flex"><span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span>Contact</a><br />
+                            <a class="link link-hover flex"><span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span>Our Services</a>
+                        </div>
+
+                        <div>
+                            <a class="link link-hover flex ml-12"> <span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span>  Classic Blog</a> <br />
+                            <a class="link link-hover flex ml-12"> <span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span> Cart</a><br />
+                            <a class="link link-hover flex ml-12"> <span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span> Contact</a><br />
+                            <a class="link link-hover flex ml-12"> <span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span> My Account</a><br />
+                            <a class="link link-hover flex ml-12"> <span className='mr-2 mt-1'><AiOutlineArrowRight></AiOutlineArrowRight></span> Shop Contact</a>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <span class="footer-title">Legal</span>
-                    <a class="link link-hover">Terms of use</a>
-                    <a class="link link-hover">Privacy policy</a>
-                    <a class="link link-hover">Cookie policy</a>
+
+                <div className='lg:ml-8'>
+                    <span style={{ backgroundColor: '#CA1F26' }} class="mb-5 w-56 h-22 p-3 font-bold text-xl text-white">TRANSPORT OFFICE</span>
+                    <p class="link link-hover"> 14 Tottenham Road, London, England.</p>
+                    <p class="link link-hover"> (102) 6666 8888</p>
+                    <p class="link link-hover"> opisarker99@gmail.com</p>
+                    <p class="link link-hover"> (102) 8888 9999</p>
+                    <p class="link link-hover"> Mon - Sat: 9:00 - 18:00</p>
                 </div>
-                <div>
-                    <span class="footer-title">Social</span>
-                    <a class="link link-hover">Twitter</a>
-                    <a class="link link-hover">Instagram</a>
-                    <a class="link link-hover">Facebook</a>
-                    <a class="link link-hover">Github</a>
-                </div>
-                <div>
-                    <span class="footer-title">Explore</span>
-                    <a class="link link-hover">Features</a>
-                    <a class="link link-hover">Enterprise</a>
-                    <a class="link link-hover">Security</a>
-                    <a class="link link-hover">Pricing</a>
-                </div>
-                <div>
-                    <span class="footer-title">Apps</span>
-                    <a class="link link-hover">Mac</a>
-                    <a class="link link-hover">Windows</a>
-                    <a class="link link-hover">iPhone</a>
-                    <a class="link link-hover">Android</a>
-                </div>
+
             </footer>
         </div>
     );
